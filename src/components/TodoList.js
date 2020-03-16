b31db004402bb8866e95696e3432c4200e089e6a
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-03-16 14:32:38 -0600

    Add button to add a new todo

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,11 +5,16 @@ import Todo from "./Todo";
 function TodoList() {
   const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
 
+  const newTodo = () => {
+    dispatch({ type: "NEW_TODO" });
+  };
+
   return (
     <div>
       {state.todos.map(todo => (
         <Todo key={todo.id} todo={todo} />
       ))}
+      <button onClick={newTodo}>New Todo</button>
     </div>
   );
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
 export const INITIAL_STATE = {
+  editing: null,
   todos: [
     { item: "Learn about reducers", completed: false, id: 1337 },
     { item: "Implement about reducers", completed: true, id: 9001 }
@@ -6,5 +7,14 @@ export const INITIAL_STATE = {
 };
 
 export function reducer(state, action) {
+  switch (action.type) {
+    case "NEW_TODO":
+      const newId = Date.now();
+      return {
+        ...state,
+        editing: newId,
+        todos: [...state.todos, { item: "", completed: false, id: newId }]
+      };
+  }
   return state;
 }
